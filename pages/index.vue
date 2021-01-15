2fa350ac964bdfbb26edd06efa979e6868b8fff1
joo6077 <joo6077@gmail.com>
2021-01-16 03:57:38 +0900

    nuxt composition api 설치 및 page 생성

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        one-line-coding
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <template>
+      <h1>Hello Nuxt!</h1>
+    </template>
   </div>
 </template>
 
